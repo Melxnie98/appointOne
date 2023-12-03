caaf455f0785303a4f0240ab1633f89e2caa7bfd
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-03 16:35:16 +0000

    fix?

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <FixedHeader title="Welcome appoint ont!" />
+        
         
         <p className="description">
           Get started by editing <code>pages/index.js</code>
