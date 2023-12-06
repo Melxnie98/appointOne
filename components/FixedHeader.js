8717a3bd4a7525cd75c2298f9c192abf2b5e6b45
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-06 14:09:06 +0000

    header buttons

--- components/FixedHeader.js
+++ components/FixedHeader.js
@@ -6,6 +6,14 @@ const FixedHeader = ({ title }) => {
   return (
     <header className={styles.fixedHeader}>
       <h1 className={styles.title}>{title}</h1>
+      <img src="/HeadImg.png" alt="Logo" className={styles.logo} />
+        <div className={styles.navigation}>
+          <div className={styles.navItem}>Home</div>
+          <div className={styles.navItem}>About</div>
+          <div className={styles.navItem}>Services</div>
+          <div className={styles.navItem}>Contact</div>
+        </div>
+      
     </header>
   );
 };
