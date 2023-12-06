0df2f6b0a2b4713f180b56bb187e4e6b38eb05c2
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-06 14:14:21 +0000

    fix

--- components/FixedHeader.js
+++ components/FixedHeader.js
@@ -6,13 +6,7 @@ const FixedHeader = ({ title }) => {
   return (
     <header className={styles.fixedHeader}>
       <h1 className={styles.title}>{title}</h1>
-      <img src="/HeadImg.png" alt="Logo" className={styles.logo} />
-        <div className={styles.navigation}>
-          <div className={styles.navItem}>Home</div>
-          <div className={styles.navItem}>About</div>
-          <div className={styles.navItem}>Services</div>
-          <div className={styles.navItem}>Contact</div>
-        </div>
+      
       
     </header>
   );
--- components/ScrollableHeader.js
+++ components/ScrollableHeader.js
@@ -5,6 +5,13 @@ const ScrollableHeader = ({ title }) => {
   return (
     <header className={styles.scrollableHeader}>
       <h1 className={styles.title}>{title}</h1>
+      <img src="/HeadImg.png" alt="Logo" className={styles.logo} />
+        <div className={styles.navigation}>
+          <div className={styles.navItem}>Home</div>
+          <div className={styles.navItem}>About</div>
+          <div className={styles.navItem}>Services</div>
+          <div className={styles.navItem}>Contact</div>
+        </div>
     </header>
   );
 };
