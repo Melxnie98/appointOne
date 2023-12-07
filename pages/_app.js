e9226a2cd5514d0f3228aca440e108c2f6e4ccba
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 18:23:08 +0000

    now?

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,17 @@ import Footer from '@components/Footer'
 function Application({ Component, pageProps }) {
   return (
     <div>
-      <div><FixedHeader title="+353 (0)83 4400999" /></div>
-      <ScrollableHeader />
-      
+      <div>
+        <div><FixedHeader title="+353 (0)83 4400999" /></div>
+        <ScrollableHeader />
+      </div>
       
       <div className="content">
         <Component {...pageProps} />
       </div>
-
+      
     </div>
+    
   );
 }
 
