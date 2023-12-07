0f23ba98847d677adfe2795733a89f3e9c512c96
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 16:51:32 +0000

    hi

--- pages/index.js
+++ pages/index.js
@@ -30,31 +30,28 @@ export default function Home() {
             <h1>Why Choose Appoint One</h1>
           </div>
           <div className="fiftyContainer">
-          <p>Choose Appoint One for a unique blend of compassion, integrity, and expertise. This is a values  driven organization where respect for the individual and working with discretion, empathy and integrity is foremost.  An authentic empathetic approach ensures personalized guidance for career transitions, conflict resolution, and HR strategy refinement and implementation. </p>
+           <p>Choose Appoint One for a unique blend of compassion, integrity, and expertise. This is a values  driven organization where respect for the individual and working with discretion, empathy and integrity is foremost.  An authentic empathetic approach ensures personalized guidance for career transitions, conflict resolution, and HR strategy refinement and implementation. </p>
           </div>
-
-           
-          
-
-        </div></div>
+        </div>
+        </div>
         <div className="greenContainer">
           <div className="innerContainer">
-            <h1>Expertise</h1>
+            <h2>Expertise</h2>
             <p>With over 30 years Human Resources Management  experience , coupled with qualifications in Career Guidance Counselling and Mediation, 
               Claire Hurley brings a wealth of expertise to guide you through career complexities, resolve disputes, and support you on HR related topics.</p>
           </div>
           <div className="innerContainer">
-            <h1>Compassion and Empathy</h1>
+            <h2>Compassion and Empathy</h2>
             <p>Claire prioritizes understanding your unique aspirations and challenges, offering unwavering support built on kindness and empathy. </p>
           </div>
         </div>
         <div className="greenContainer">
           <div className="innerContainer">
-            <h1>Integrity and Transparency</h1>
+            <h2>Integrity and Transparency</h2>
             <p>Trust is at the core of all interactions. You can rely on honesty, transparency, and commitment to your best interests. You will be empowered with the knowledge and tools to make informed decisions.</p>
           </div>
           <div className="innerContainer">
-            <h1>Tailored Solutions</h1>
+            <h2>Tailored Solutions</h2>
             <p>My strengths lie in my capacity for empathy  which allows me to create personalized solutions that fit your needs, whether you're at a career crossroads, facing workplace conflicts, or in need of HR guidance.</p>
           </div>
         </div>
