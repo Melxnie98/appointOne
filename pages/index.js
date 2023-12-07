52f210ceb313bcf18187c4edec67fc752ac53a41
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 15:22:33 +0000

    pllssss

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,10 @@ export default function Home() {
           </div> 
         </div>
         <div className="whyContainer">
-          <div class="overlay">
+          
             <h1>Why Choose Appoint One</h1>
            <p>Choose Appoint One for a unique blend of compassion, integrity, and expertise. This is a values  driven organization where respect for the individual and working with discretion, empathy and integrity is foremost.  An authentic empathetic approach ensures personalized guidance for career transitions, conflict resolution, and HR strategy refinement and implementation. </p>
-          </div>
+          
 
         </div>
         <div className="greenContainer">
