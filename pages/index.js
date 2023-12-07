1a6e939f021c11d2a3ccba9a291b440474bedd5a
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 15:07:16 +0000

    innercontainercss

--- pages/index.js
+++ pages/index.js
@@ -30,22 +30,22 @@ export default function Home() {
 
         </div>
         <div className="greenContainer">
-          <div className="Container">
+          <div className="innerContainer">
             <h1>Expertise</h1>
             <p>With over 30 years Human Resources Management  experience , coupled with qualifications in Career Guidance Counselling and Mediation, 
               Claire Hurley brings a wealth of expertise to guide you through career complexities, resolve disputes, and support you on HR related topics.</p>
           </div>
-          <div className="Container">
+          <div className="innerContainer">
             <h1>Compassion and Empathy</h1>
             <p>Claire prioritizes understanding your unique aspirations and challenges, offering unwavering support built on kindness and empathy. </p>
           </div>
         </div>
         <div className="greenContainer">
-          <div className="Container">
+          <div className="innerContainer">
             <h1>Integrity and Transparency</h1>
             <p>Trust is at the core of all interactions. You can rely on honesty, transparency, and commitment to your best interests. You will be empowered with the knowledge and tools to make informed decisions.</p>
           </div>
-          <div className="Container">
+          <div className="innerContainer">
             <h1>Tailored Solutions</h1>
             <p>My strengths lie in my capacity for empathy  which allows me to create personalized solutions that fit your needs, whether you're at a career crossroads, facing workplace conflicts, or in need of HR guidance.</p>
           </div>
