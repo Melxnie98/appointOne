cf727df9b46bde09d7465ecd930f543a58147984
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 12:19:00 +0000

    aboutContent

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <div></div>
+        <div className="greenContainer">
+          <img src="/team.png" alt="Team"/>
+          <h2>Your Trusted Guide for Personal and Professional Growth</h2>
+          <h1>Empowerment and Resolution with Claire Hurley</h1>
+          Welcome to Appoint One founded by Claire Hurley, a seasoned HR Professional, qualified Mediator and qualified Career Guidance Counsellor. The aim of Appoint One is to partner with individuals, families and organizations to empower them to take the next step, that is theirs to take, 
+in the resolution of a conflict, the provision of a harmonious work culture/environment or make career decisions.
+        </div>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
