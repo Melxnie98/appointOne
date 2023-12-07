d5c8d6b46b1b5544b4d66c186ffbe3eb85aa6a8c
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 15:09:36 +0000

    pls

--- components/Footer.js
+++ components/Footer.js
@@ -11,11 +11,11 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
 
-        <div className="Container" >
+        <div className="innerContainer" >
           <img src="/HeadImg.png" alt="Appoint One Logo" className={styles.logo} />
           <p>At Appoint One, we’re your partners in personal and professional growth. With compassion, integrity, and empathy as our cornerstones, we guide you through career complexities, resolve challenges, and optimize HR strategies, all with expertise and care.</p>
         </div>
-        <div className="Container" >
+        <div className="innerContainer" >
           <h1>Quick Links</h1>
           <div onClick={() => navigateTo('/index')}>Home</div>
           <div onClick={() => navigateTo('/services')}>Services</div>
@@ -23,7 +23,7 @@ export default function Footer() {
           <div onClick={() => navigateTo('/pricing')}>Pricing</div>
           <div onClick={() => navigateTo('/contact')}>Contact Us</div>
         </div>
-        <div className="Container" >
+        <div className="innerContainer" >
           <h1>Contact us</h1>
         </div>
         
