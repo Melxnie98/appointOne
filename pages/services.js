3496a886942a271e490a010691d1d4c683dc603a
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 17:02:39 +0000

    stuff

--- pages/services.js
+++ pages/services.js
@@ -11,14 +11,14 @@ const ServicesPage = () => {
       </Head>
 
       <main>
-      <div className="servicesContainer">
       <div className="overlay">
-        <div className="fiftyContainer">
-            <h1>Unlocking Opportunities</h1>
-        </div>
+        <div className="servicesContainer">
+            <div className="fiftyContainer">
+                <h1>Unlocking Opportunities</h1>
+            </div>
         <div className="fiftyContainer">
             <h2>Explore the Range of Services at Appoint One</h2>
-        <p>Welcome to Appoint One’s service page, where we invite you to delve into a world of possibilities and solutions. Our commitment to excellence and transformation is reflected in the comprehensive array of services we offer.</p>
+            <p>Welcome to Appoint One’s service page, where we invite you to delve into a world of possibilities and solutions. Our commitment to excellence and transformation is reflected in the comprehensive array of services we offer.</p>
         </div>
         </div>
       </div>
