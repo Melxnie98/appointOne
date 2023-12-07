cbcc23bd622b3427e15bd05350176ff5830ee853
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 11:52:30 +0000

    footer stuff

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,12 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
+        <div className={styles.across}>
+          <img src="/HeadImg.png" alt="Appoint One Logo" className={styles.logo} />
+          At Appoint One, we’re your partners in personal and professional growth. With compassion, integrity, and empathy as our cornerstones, we guide you through career complexities, resolve challenges, and optimize HR strategies, all with expertise and care
+        </div>
         <div>
-        <img src="/HeadImg.png" alt="Appoint One Logo" className={styles.logo} />
-        At Appoint One, we’re your partners in personal and professional growth. With compassion, integrity, and empathy as our cornerstones, we guide you through career complexities, resolve challenges, and optimize HR strategies, all with expertise and care
+          ji
         </div>
         
       </footer>
