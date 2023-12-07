73c53cdecb06cec8c3d1431351f552282c649a64
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 13:18:33 +0000

    content

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,13 @@ const AboutPage = () => {
       <main>
 
         <p className="description">
-          About <code>pages/about.js</code>
+        About me 
+I established Appoint One with a clear vision - to provide support and inspiration to individuals and organizations, enabling them to effectively address complex challenges and progress in a positive direction. In so doing, be fulfilled in the work that I do.  My value is in HR problem solving, conflict resolution, mediation and facilitation, employee relations, and career guidance for adults seeking a change. I offer a wide range of services to support positive workplace relations in organizations and will guide employers and employees through difficult work situations be it a grievance, investigation, conflict or complex disciplinary issue. I offer workplace, community and family mediation including child inclusive mediation.  I work with integrity and discretion.  I approach situations with a  solution focused mindset prioritizing trust and transparency of communication. 
+I am an accredited Mediator with the Mediators Institute Ireland(MII)  Associate of Chartered Institute Personnel And Development (CIPD) and accredited member of  Institute Guidance Counsellors( IGC)
+My professional journey spans over three decades, during which I have held senior HR roles in both the IT and Medical Device sectors. This substantial experience is complemented by over eight years of dedicated service in the field of career guidance. I have also garnered valuable insights and expertise during my tenure in the public sector, where I occupied regional roles that facilitated collaboration between Industry, Education, and State Agencies. These roles were instrumental in the identification of skills gaps and the implementation of strategies to address them. My strengths and skills have been honed through extensive involvement in change management, organizational restructuring, conflict resolution, facilitation, mediation, and career coaching activities.
+My interests lie in the outdoors kayaking, open water swimming and hillwalking.  Generally love life and approach everything with an optimistic outlook.
+
+        
         </p>
       </main>
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,15 @@ export default function Home() {
             Welcome to Appoint One founded by Claire Hurley, a seasoned HR Professional, qualified Mediator and qualified Career Guidance Counsellor. The aim of Appoint One is to partner with individuals, families and organizations to empower them to take the next step, that is theirs to take, in the resolution of a conflict, the provision of a harmonious work culture/environment or make career decisions. 
           </div> 
         </div>
+        <div>
+          <h1>Why Choose Appoint One</h1>
+          Choose Appoint One for a unique blend of compassion, integrity, and expertise. This is a values  driven organization where respect for the individual and working with discretion, empathy and integrity is foremost.  An authentic empathetic approach ensures personalized guidance for career transitions, conflict resolution, and HR strategy refinement and implementation. 
+
+        </div>
+        <div>
+          <h1>Expertise</h1>
+         With over 30 years Human Resources Management  experience , coupled with qualifications in Career Guidance Counselling and Mediation, Claire Hurley brings a wealth of expertise to guide you through career complexities, resolve disputes, and support you on HR related topics.
+        </div>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
