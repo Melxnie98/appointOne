20cb82804fd6b13d3f95616ac7e352592a22a1be
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 11:57:34 +0000

    more footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,20 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <div className={styles.across}>
+        <div>
           <img src="/HeadImg.png" alt="Appoint One Logo" className={styles.logo} />
           At Appoint One, we’re your partners in personal and professional growth. With compassion, integrity, and empathy as our cornerstones, we guide you through career complexities, resolve challenges, and optimize HR strategies, all with expertise and care
         </div>
         <div>
-          ji
+          <h1>Quick Links</h1>
+          <div onClick={() => navigateTo('/index')}>Home</div>
+          <div onClick={() => navigateTo('/services')}>Services</div>
+          <div onClick={() => navigateTo('/about')}>About Us</div>
+          <div onClick={() => navigateTo('/pricing')}>Pricing</div>
+          <div onClick={() => navigateTo('/contact')}>Contact Us</div>
+        </div>
+        <div>
+          Contact us
         </div>
         
       </footer>
