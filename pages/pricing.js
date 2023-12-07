8bc7736a9ed5d83ef80598299edd31ccdc7fc37d
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 20:32:18 +0000

    p

--- pages/pricing.js
+++ pages/pricing.js
@@ -21,11 +21,11 @@ const PricingPage = () => {
           </div>
         </div>
       </div>
-      <div className="lightGreenContainer">
-        <div className="innerContainer">
-           <img className="transformationImg"src="/transformations.jpg" alt="Team"/>
+      <div className="contentContainer">
+        <div className="fiftyContainer">
+           <img src="/transformations.jpg" alt="Team"/>
           </div>
-          <div className="innerContainer">
+          <div className="fiftyContainer">
             <h2>Our Commitment To Excellence And Transformation</h2>
             <p>At Appoint One, we are dedicated to delivering excellence in every aspect of our service. With over 30 years of experience in Human Resources, Careers Coaching, and Mediation, we have honed our expertise to guide you on your journey to personal and professional growth. Our commitment to compassion, integrity, and empathy forms the foundation of our approach. Whether you seek resolution through mediation, career development guidance, or HR solutions, our team is here to provide unwavering support. Your success is our success, and we look forward to partnering with you in achieving your goals.</p>
             <p>Your success is our ultimate reward, and we are committed to being your dedicated partner on your journey to empowerment and resolution. With Appoint One, you’re not just a client; you’re a valued part of our community, and we look forward to embarking on this transformative journey with you.</p>
