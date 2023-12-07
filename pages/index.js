6adcf245281c5c9290085fe1112aa798aa4331b2
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 15:20:15 +0000

    pls

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,11 @@ export default function Home() {
             <button onClick={() => navigateTo('/contact')}>Contact Us</button>
           </div> 
         </div>
-        <div className="contentContainer">
-        <div class="overlay"></div>
-          <h1>Why Choose Appoint One</h1>
-          <p>Choose Appoint One for a unique blend of compassion, integrity, and expertise. This is a values  driven organization where respect for the individual and working with discretion, empathy and integrity is foremost.  An authentic empathetic approach ensures personalized guidance for career transitions, conflict resolution, and HR strategy refinement and implementation. </p>
+        <div className="whyContainer">
+          <div class="overlay">
+            <h1>Why Choose Appoint One</h1>
+           <p>Choose Appoint One for a unique blend of compassion, integrity, and expertise. This is a values  driven organization where respect for the individual and working with discretion, empathy and integrity is foremost.  An authentic empathetic approach ensures personalized guidance for career transitions, conflict resolution, and HR strategy refinement and implementation. </p>
+          </div>
 
         </div>
         <div className="greenContainer">
