e09d086c7108d7367060bff09babf1dd2d6c3b59
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-07 12:39:30 +0000

    scroll

--- components/ScrollableHeader.js
+++ components/ScrollableHeader.js
@@ -1,24 +1,53 @@
+// components/ScrollableHeader.js
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './ScrollableHeader.module.css';
 
 const ScrollableHeader = ({ title }) => {
   const router = useRouter();
+  const [isHeaderAtTop, setIsHeaderAtTop] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
   const navigateTo = (path) => {
     router.push(path);
   }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = typeof window !== 'undefined' ? window.pageYOffset : 0;
+
+      if (prevScrollPos > currentScrollPos) {
+        // Scrolling up, show the header at the top
+        setIsHeaderAtTop(true);
+      } else {
+        // Scrolling down, move the header to the top of the screen
+        setIsHeaderAtTop(currentScrollPos === 0);
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    if (typeof window !== 'undefined') {
+      // Ensure that the code runs only in the browser, not during server-side rendering
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, [prevScrollPos]);
+
   return (
-    <header className={styles.scrollableHeader}>
+    <header className={`${styles.scrollableHeader} ${isHeaderAtTop ? styles.atTop : ''}`}>
       <h1 className={styles.title}>{title}</h1>
       <img src="/HeadImg.png" alt="Logo" className={styles.logo} onClick={() => navigateTo('/index')}/>
-        <div className={styles.navigation}>
-          <div className={styles.navItem}onClick={() => navigateTo('/index')}>Home</div>
-          <div className={styles.navItem}onClick={() => navigateTo('/services')}>Services</div>
-          <div className={styles.navItem}onClick={() => navigateTo('/about')}>About Us</div>
-          <div className={styles.navItem}onClick={() => navigateTo('/pricing')}>Pricing</div>
-          <div className={styles.navItem}onClick={() => navigateTo('/contact')}>Contact Us</div>
-        </div>
+      <div className={styles.navigation}>
+        <div className={styles.navItem} onClick={() => navigateTo('/index')}>Home</div>
+        <div className={styles.navItem} onClick={() => navigateTo('/services')}>Services</div>
+        <div className={styles.navItem} onClick={() => navigateTo('/about')}>About Us</div>
+        <div className={styles.navItem} onClick={() => navigateTo('/pricing')}>Pricing</div>
+        <div className={styles.navItem} onClick={() => navigateTo('/contact')}>Contact Us</div>
+      </div>
     </header>
   );
 };
