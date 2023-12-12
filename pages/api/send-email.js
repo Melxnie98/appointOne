7727009c811d2742ea2aebd08723e90c2db4765e
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-12 14:24:34 +0000

    try now

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,38 +1,69 @@
-// pages/api/send-email.js
+// functions/send-email.js
 
 import nodemailer from 'nodemailer';
 
-export default async (req, res) => {
-  if (req.method === 'POST') {
-    const { to, subject, text } = req.body;
-
-    // Configure nodemailer with your Hotmail email service credentials
-    const transporter = nodemailer.createTransport({
-      host: 'smtp-mail.outlook.com',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: 'melanieleonard98@hotmail.com',
-        pass: 'Ilovegooch12',
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'OPTIONS') {
+    // Handle preflight request
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '86400', // 1 day
       },
+      body: JSON.stringify({ success: true }),
+    };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method Not Allowed' }),
+    };
+  }
+
+  const { to, subject, text } = JSON.parse(event.body);
+
+  // Configure nodemailer with your Hotmail/Outlook email service credentials
+  const transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'melanieleonard98@hotmail.com',
+      pass: 'Ilovegooch12',
+    },
+  });
+
+  try {
+    // Send email
+    await transporter.sendMail({
+      from: 'melanieleonard98@hotmail.com',
+      to,
+      subject,
+      text,
     });
 
-    try {
-      // Send email
-      await transporter.sendMail({
-        from: 'melanieleonard98@hotmail.com',
-        to,
-        subject,
-        text,
-      });
-
-      res.status(200).json({ success: true });
-    } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ success: false, error: 'Failed to send email' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ success: true }),
+    };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ success: false, error: 'Failed to send email' }),
+    };
   }
 };
 
