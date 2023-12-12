666e52e73db096aa3cfe291a26c70daf442d5e73
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-12 10:22:49 +0000

    fixed header logos

--- components/FixedHeader.js
+++ components/FixedHeader.js
@@ -6,8 +6,13 @@ const FixedHeader = ({ title }) => {
 
   return (
     <header className={styles.fixedHeader} >
-      <img src="/phone.png" alt="Phone" className={styles.icons}/>
+      <img src="/logos/phone.png" alt="Phone" className={styles.icons}/>
       <h1 className={styles.title}>{title}</h1>
+      <img src="/logos/facebookLogo.png" alt="Facebook" className={styles.icons}/>
+      <img src="/logos/twitterLogo.png" alt="Twitter" className={styles.icons}/>
+      //https://icons8.com/icons/set/instagram--white
+      <img src="/logos/instagramLogo.png" alt="Twitter" className={styles.icons}/>
+
     </header>
   );
 };
