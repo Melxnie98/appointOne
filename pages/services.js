01c274944a37bd983592481b4a99469fc5a9aea2
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-12 13:33:42 +0000

    cleanup css

--- pages/services.js
+++ pages/services.js
@@ -76,14 +76,14 @@ const ServicesPage = () => {
        
         </div>
         <div className="lightGreenContainer">
-        <div className="fiftyGreenContainer">
-            <p> Questions?</p>
-            <h2>Call Now</h2>
-            <p>Ready to take the next step? Call now and let’s embark on your journey to empowerment and resolution.</p>
-            <h2>+353 (0)83 4400999</h2>
-        <button className="round-button" onClick={() => navigateTo('/contact')}>Book an Appointment</button>
-            
-        </div>
+          <div className="fiftyGreenContainer">
+              <p> Questions?</p>
+              <h2>Call Now</h2>
+              <p>Ready to take the next step? Call now and let’s embark on your journey to empowerment and resolution.</p>
+              <h2>+353 (0)83 4400999</h2>
+              <button className="round-button" onClick={() => navigateTo('/contact')}>Book an Appointment</button>
+              
+          </div>
         <div className="fiftyGreenContainer">
             <img className="transformationImg" src="/AONEpc.png" alt="Team"/>
         </div>
