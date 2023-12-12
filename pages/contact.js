dd3a38d3f7bd042f2cd376f5477e1ad53ce01786
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-12 13:20:15 +0000

    stuff

--- pages/contact.js
+++ pages/contact.js
@@ -20,9 +20,12 @@ const ContactPage = () => {
           </div>
         </div>
       </div>
-      
+      <div className="container">
+        <div className="fiftyContainer">We’d love to hear from you! Whether you have questions, need assistance, or want to explore how we can help you achieve your goals, please don’t hesitate to reach out. Your journey to empowerment and resolution begins with a simple conversation. Contact us today, and let’s start this transformative dialogue together</div>
+        <div className="fiftyContainer"><ContactForm /></div>
+      </div>
 
-      <ContactForm />
+      
       <div className="greenContainer">
       <h1>" As I live each day may it be my goal to encourage one mind and inspire one soul. As I live each day may I do my part to make one difference and touch one heart "</h1>
       </div>
