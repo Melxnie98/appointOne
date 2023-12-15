df7014478f3383d101af42096852ec23f9686c84
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-15 22:04:21 +0000

    phone

--- components/FixedHeader.js
+++ components/FixedHeader.js
@@ -7,8 +7,8 @@ const FixedHeader = ({ title }) => {
   return (
     <header className={styles.fixedHeader} >
        <div className={styles.navigation}>
-        <img src="/logos/phone.png" alt="Phone" className={styles.icons}/>
-        <h3 >{title}</h3>
+        <img href="tel:+353834400999" src="/logos/phone.png" alt="Phone" className={styles.icons}/>
+        
         <img src="/logos/facebookLogo.png" alt="Facebook"  className={`${styles.navItem} ${styles.icons}`}/>
         <img src="/logos/twitterLogo.png" alt="Twitter"  className={`${styles.navItem} ${styles.icons}`}/>
         <img src="/logos/instagramLogo.png" alt="Instagram" className={`${styles.navItem} ${styles.icons}`}/>
