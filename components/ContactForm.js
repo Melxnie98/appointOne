c35c04f9103e4399fc06aeb97d446102d5514c48
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-15 15:36:33 +0000

    hi

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,26 +1,69 @@
 import { useState } from 'react';
+import React from 'react';
+import { useRouter } from 'next/router';
+
+export default function Footer() {
+  const router = useRouter();
+
+  const navigateTo = (path) => {
+    router.push(path);
+    
+  }
 
 const ContactForm = () => {
-  
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    setSubmitting(true);
+
+    try {
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      });
+
+      console.log("Form successfully submitted");
+      // Optionally, you can add code to handle successful submission
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      // Optionally, you can add code to handle submission failure
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
-    <form name="contact" method="POST" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-    <label>Your Number: <input type="text" name="phone" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
+    <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+      <p>
+        <label>
+          Name: <input type="text" name="name" />
+        </label>
+        <label>
+          Phone Number: <input type="number" name="number" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Sending...' : 'Send'}
+        </button>
+      </p>
+    </form>
   );
 };
 
