4fe3c1ed39235cd3043e84702d3c98d87c5dc953
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-15 16:33:32 +0000

    gi

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,10 +1,35 @@
 import { useState } from 'react';
 
 const ContactForm = () => {
- 
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    setSubmitting(true);
+
+    try {
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      });
+
+      console.log("Form successfully submitted");
+      // Optionally, you can add code to handle successful submission
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      // Optionally, you can add code to handle submission failure
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
-    <form name="ContactForm" method="POST" data-netlify="true" action="/">
+    <form name="ContactForm" method="POST" data-netlify="true" action="">
       <p>
         <label>
           Name: <input type="text" name="name" />
