56ece591da7cb408369375681fc8a7249fa93cf4
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-19 16:18:40 +0000

    h

--- pages/mediation.js
+++ pages/mediation.js
@@ -15,20 +15,13 @@ const MediationPage = () => {
       </Head>
 
       <main>
-      <div className="contentContainer">
-        <h2>Mediation</h2>
-        <p>Mediation is defined by the Meditation Act 2017 as ‘A confidential, facilitative and voluntary process in which parties to a dispute, with the assistance of a mediator , attempt to reach a mutually acceptable agreement to resolve the dispute’</p>
-        <p>Claire is an accredited mediator with the Mediators Institute Ireland. The practice of Appoint one is bound by the Mediation Act 2017 and the Mediators Institute Ireland Code of Practice.</p>
-        <p>The aim of mediation is to assist the client and /or parties to prevent or resolve a broad range of disputes or conflicts within a variety of settings and to facilitate improvement to future relationships.</p>
-      </div>
+     
       <h2>Available for all mediation needs with particular focus on:</h2>
       <div className="servicesContainer">
         <div className="overlay">
             <div className="fiftyContainer">
                 <h1>Mediation Services</h1>     
-                <h2>Explore the Range of Services at Appoint One</h2>
-                <p>Welcome to Appoint One’s service page, where we invite you to delve into a world of possibilities and solutions. 
-                    Our commitment to excellence and transformation is reflected in the comprehensive array of services we offer.</p> 
+                
                 <button className="green-button" onClick={() => navigateTo('/contact')}>Book an Appointment</button>
             </div>
         </div>
