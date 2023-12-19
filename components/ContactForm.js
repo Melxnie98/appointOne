1bc2cd528da6389b0a9cdedd1dba1317cace30a6
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-19 12:58:11 +0000

    hi

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { navigate } from 'gatsby'; // Import navigate from Gatsby
 import styles from './ContactForm.module.css';
-
 
 const ContactForm = () => {
   const [isSubmitting, setSubmitting] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  
+
     const myForm = event.target;
     const formData = new FormData(myForm);
-  
+
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -19,13 +19,18 @@ const ContactForm = () => {
       .then(() => navigate("/thank-you/"))
       .catch((error) => alert(error));
   };
-  
 
   return (
-  
-    <form className={styles.contact-form} name="ContactForm" method="POST" data-netlify="true" action="/" >
-       <input type="hidden" name="form-name" value="ContactForm" />
-      <p className={styles.input-group}>
+    <form
+      className={styles['contact-form']} // Use bracket notation for dynamic class names
+      name="ContactForm"
+      method="POST"
+      data-netlify="true"
+      action="/"
+      onSubmit={handleSubmit} // Add onSubmit handler to the form
+    >
+      <input type="hidden" name="form-name" value="ContactForm" />
+      <p className={styles['input-group']}>
         <label>
           Name: <input type="text" name="name" />
         </label>
@@ -43,15 +48,19 @@ const ContactForm = () => {
           Message: <textarea name="message"></textarea>
         </label>
       </p>
-      <p >
-        <button className={styles.submit-button} type="submit" disabled={isSubmitting}>
+      <p>
+        <button
+          className={styles['submit-button']}
+          type="submit"
+          disabled={isSubmitting}
+        >
           {isSubmitting ? 'Sending...' : 'Send'}
         </button>
       </p>
     </form>
- 
   );
 };
 
 export default ContactForm;
+
 
