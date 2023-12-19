191078741355479146b3966ee4154823091cdb59
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-19 13:00:27 +0000

    hi

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -5,62 +5,68 @@ import styles from './ContactForm.module.css';
 const ContactForm = () => {
   const [isSubmitting, setSubmitting] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const myForm = event.target;
-    const formData = new FormData(myForm);
+    try {
+      const myForm = document.forms['ContactForm'];
+      const formData = new FormData(myForm);
 
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => navigate("/thank-you/"))
-      .catch((error) => alert(error));
+      await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(formData).toString(),
+      });
+
+      // Assuming navigate is used for client-side navigation
+      navigate('/thank-you/');
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      // Handle error, show an alert, etc.
+    }
   };
 
   return (
-    <form
-      className={styles['contact-form']} // Use bracket notation for dynamic class names
-      name="ContactForm"
-      method="POST"
-      data-netlify="true"
-      action="/"
-      onSubmit={handleSubmit} // Add onSubmit handler to the form
-    >
-      <input type="hidden" name="form-name" value="ContactForm" />
-      <p className={styles['input-group']}>
-        <label>
-          Name: <input type="text" name="name" />
-        </label>
-        <label>
-          Phone Number: <input type="number" name="number" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button
-          className={styles['submit-button']}
-          type="submit"
-          disabled={isSubmitting}
-        >
-          {isSubmitting ? 'Sending...' : 'Send'}
-        </button>
-      </p>
-    </form>
+    <div className={styles['contact-form']}>
+      <form
+        name="ContactForm"
+        method="POST"
+        data-netlify="true"
+        action="/"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="ContactForm" />
+        <p className={styles['input-group']}>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+          <label>
+            Phone Number: <input type="number" name="number" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button
+            className={styles['submit-button']}
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Sending...' : 'Send'}
+          </button>
+        </p>
+      </form>
+    </div>
   );
 };
 
 export default ContactForm;
-
 
