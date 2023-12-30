ad6e5423377e4238c042a6da964f102513255035
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-30 16:53:51 +0000

    maybeee

--- pages/index.js
+++ pages/index.js
@@ -43,18 +43,18 @@ export default function Home() {
         </div>
         </div>
         <div className="greenContainer">
-          <div className="greenContainer">
-            <div className="lilContainer">
+          <div className="lilContainer">
+            <div className="innerContainer">
               <h2>Expertise</h2>
               <p>With over 30 years Human Resources Management  experience , coupled with qualifications in Career Guidance Counselling and Mediation, 
                 Claire Hurley brings a wealth of expertise to guide you through career complexities, resolve disputes, and support you on HR related topics.</p>
             </div>
-            <div className="lilContainer">
+            <div className="innerContainer">
               <h2>Compassion and Empathy</h2>
               <p>Claire prioritizes understanding your unique aspirations and challenges, offering unwavering support built on kindness and empathy. </p>
             </div>
             </div>
-            <div className="greenContainer">
+            <div className="lilContainer">
           
             <div className="innerContainer">
               <h2>Integrity and Transparency</h2>
@@ -88,7 +88,7 @@ export default function Home() {
               <button className="green-button" onClick={() => navigateTo('/contact')}>Contact Us</button>
             </div> 
           </div>
-        </div>
+          </div>
         
         </div>
       </main>
