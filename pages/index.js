a9f1d2ea0a2481b52151579322e4ceb69606bf36
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2023-12-30 16:48:31 +0000

    hi

--- pages/index.js
+++ pages/index.js
@@ -44,12 +44,12 @@ export default function Home() {
         </div>
         <div className="greenContainer">
           <div className="greenContainer">
-            <div className="innerContainer">
+            <div className="lilContainer">
               <h2>Expertise</h2>
               <p>With over 30 years Human Resources Management  experience , coupled with qualifications in Career Guidance Counselling and Mediation, 
                 Claire Hurley brings a wealth of expertise to guide you through career complexities, resolve disputes, and support you on HR related topics.</p>
             </div>
-            <div className="innerContainer">
+            <div className="lilContainer">
               <h2>Compassion and Empathy</h2>
               <p>Claire prioritizes understanding your unique aspirations and challenges, offering unwavering support built on kindness and empathy. </p>
             </div>
