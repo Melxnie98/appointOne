e2fed0ea77f0b4a29ee8441ee6528536a96363d0
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-01-22 13:26:16 +1300

    pls

--- pages/services.js
+++ pages/services.js
@@ -11,13 +11,15 @@ const ServicesPage = () => {
   const navigateTo = (path) => {
     router.push(path);
   }
-  function openTab(tabName) {
-    const tabs = document.getElementsByClassName('tab-content');
-    for (const tab of tabs) {
-      tab.style.display = 'none';
+  function showTab(tabId) {
+    // Hide all tab contents
+    var tabContents = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabContents.length; i++) {
+      tabContents[i].style.display = 'none';
     }
-  
-    document.getElementById(tabName).style.display = 'block';
+
+    // Show the selected tab content
+    document.getElementById(tabId).style.display = 'block';
   }
   return (
     <div className="container">
@@ -42,25 +44,22 @@ const ServicesPage = () => {
       </div>
       <div className="contentContainer">
       <div class="tab-container">
-    <button class="tab" onclick="openTab('tab1')">Tab 1</button>
-    <button class="tab" onclick="openTab('tab2')">Tab 2</button>
-    <button class="tab" onclick="openTab('tab3')">Tab 3</button>
+  <div class="tab" onclick="showTab('tab1')">Tab 1</div>
+  <div class="tab" onclick="showTab('tab2')">Tab 2</div>
+  <div class="tab" onclick="showTab('tab3')">Tab 3</div>
+</div>
 
-    <div id="tab1" class="tab-content">
-      <h2>Tab 1 Content</h2>
-      <p>This is the content for Tab 1.</p>
-    </div>
+<div class="tab-content" id="tab1">
+  <p>Content for Tab 1 goes here.</p>
+</div>
 
-    <div id="tab2" class="tab-content">
-      <h2>Tab 2 Content</h2>
-      <p>This is the content for Tab 2.</p>
-    </div>
+<div class="tab-content" id="tab2">
+  <p>Content for Tab 2 goes here.</p>
+</div>
 
-    <div id="tab3" class="tab-content">
-      <h2>Tab 3 Content</h2>
-      <p>This is the content for Tab 3.</p>
-    </div>
-  </div>
+<div class="tab-content" id="tab3">
+  <p>Content for Tab 3 goes here.</p>
+</div>
   </div>
      
       <div className="contentContainer">
